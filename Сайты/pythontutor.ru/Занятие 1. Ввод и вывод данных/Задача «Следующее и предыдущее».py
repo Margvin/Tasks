997c9cf4bb8d0d7c8352003ed412504ff1f504e1
Margvin <86877013+Margvin@users.noreply.github.com>
2021-07-25 01:51:03 +0500

    25.07

---
+++ Сайты/pythontutor.ru/Занятие 1. Ввод и вывод данных/Задача «Следующее и предыдущее».py
@@ -0,0 +1,10 @@
+#   Условие
+#   Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы
+#   важны!).
+
+#   Запрашиваем число
+n = int(input())
+
+#   Выводим
+print ("The next number for the number " + str(n) + " is " + str(n + 1))
+print ("The previous number for the number " + str(n) + " is " + str(n - 1))
